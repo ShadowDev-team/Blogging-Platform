ad13980fb06d50ab8b6f03bb0d90190b5636c4ad
Anass-Dr <anasdrissi77@gmail.com>
2024-09-09 16:27:10 +0100

    add necessary packages for the project

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const session = require('express-session');
+require("./config/database");
+
 const apiRoutes = require('./routes/api');
 const webRoutes = require('./routes/web');
 
+
 const app = express();
+app.set('view engine', 'ejs');
 
 // middlewares
+app.use(session({
+    secret: 'secret-key',
+    resave: false,
+    saveUninitialized: true
+}));
+app.use(express.json());
+app.use(expressLayouts);
 app.use(express.static('public'));
-app.set('view engine', 'ejs');
 app.use("/api", apiRoutes);
 app.use("/", webRoutes);
 
