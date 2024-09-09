46f98927a1966f94ec8dd8dd1b68566f542b8e03
Anass-Dr <anasdrissi77@gmail.com>
2024-09-09 19:13:14 +0100

    fix: models (createdAt, updatedAt) attributes

--- models/Article.js
+++ models/Article.js
@@ -39,16 +39,6 @@ module.exports = (sequelize, DataTypes) => {
             type: DataTypes.TEXT,
             allowNull: false,
         },
-        createdAt: {
-            type: DataTypes.DATE,
-            allowNull: false,
-            defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-        },
-        updatedAt: {
-            type: DataTypes.DATE,
-            allowNull: false,
-            defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-        }
     }, {
         sequelize,
         modelName: 'Article',
