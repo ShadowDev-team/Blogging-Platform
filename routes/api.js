26ecf1d704384f384bbc20475631a61795660efb
fakihiya <yasinfakihi21@gmail.com>
2024-09-11 19:20:49 +0100

    in this commit i can display comments for every articl

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+
+
+
+const CommentController = require('../controllers/CommentController');
+
+router.post('/comments', CommentController.create);
+router.get('/articles/:article_id/comments', CommentController.getByArticle);
+// router.get('/articles/:articleId/comments', CommentController.getCommentsByArticleId);
+
+// router.put('/comments/:id', CommentController.update);
+// router.delete('/comments/:id', CommentController.delete);
+
+
 module.exports = router;
