86fd8be963dca98f92a3e37cccb86a0a54748915
aymanebs <aymane.bisdaoune@gmail.com>
2024-09-12 17:20:37 +0100

    integrated articles in profile

--- migrations/20240910135328-add-profile-picture-to-users.js
+++ migrations/20240910135328-add-profile-picture-to-users.js
@@ -3,13 +3,13 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.addColumn('Users', 'profilePicture', {
+    await queryInterface.addColumn('users', 'profilePicture', {
       type: Sequelize.STRING,
       allowNull: true,
     });
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.removeColumn('Users', 'profilePicture');
+    await queryInterface.removeColumn('users', 'profilePicture');
   }
 };
